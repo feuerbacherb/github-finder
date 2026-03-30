46a57225f77f9e1cc3b2056f451b3d367e971503
Brent Feuerbacher <feuerbacherb@gmail.com>
2026-03-30 14:14:47 -0500

    created workable search and button combo that returns GitHub information for a found user, displaying the information on a user-card

---
+++ src/components/UserSearch.tsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+const UserSearch = () => {
+  const [username, setUsername] = useState("");
+  const [submittedUsername, setSubmittedUsername] = useState("");
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["users", submittedUsername],
+    queryFn: async () => {
+      const res = await fetch(`${import.meta.env.VITE_GITHUB_API_URL}/users/${submittedUsername}`);
+
+      if (!res.ok) {
+        throw new Error("User not found");
+      }
+
+      const data = await res.json();
+      console.log(data);
+
+      return data;
+    },
+    enabled: !!submittedUsername,
+  });
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(`Submitted username: ${username}`);
+    setSubmittedUsername(username.trim());
+  };
+
+  return (
+    <>
+      <form
+        className='form'
+        onSubmit={handleSubmit}>
+        <input
+          type='text'
+          placeholder='Enter GitHub Username...'
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button type='submit'>Search</button>
+      </form>
+      {isLoading && <p className='status'>Loading...</p>}
+      {isError && <p className='status error'>{error.message}</p>}
+
+      {data && (
+        <div className='user-card'>
+          <img
+            src={data.avatar_url}
+            alt={data.name}
+            className='avatar'
+          />
+          <h2>{data.name || data.login}</h2>
+          <p>{data.bio}</p>
+          <a
+            href={data.html_url}
+            className='profile-btn'
+            target='_blank'
+            rel='noopener noreferrer'>
+            View Github Profile
+          </a>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default UserSearch;
