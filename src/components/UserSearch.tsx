f910518a82190762d26b87149c8f4ac75a88a67c
Brent Feuerbacher <feuerbacherb@gmail.com>
2026-03-31 08:44:47 -0500

    cleaned up code by separating the fetch function and creating a UserCard component; also created the types.ts and included GitHubUser as a type

--- src/components/UserSearch.tsx
+++ src/components/UserSearch.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useQuery } from "@tanstack/react-query";
-import { FaGithubAlt } from "react-icons/fa";
+import { fetchGithubUser } from "../api/github";
+import UserCard from "./UserCard";
 
 const UserSearch = () => {
   const [username, setUsername] = useState("");
@@ -8,18 +9,7 @@ const UserSearch = () => {
 
   const { data, isLoading, isError, error } = useQuery({
     queryKey: ["users", submittedUsername],
-    queryFn: async () => {
-      const res = await fetch(`${import.meta.env.VITE_GITHUB_API_URL}/users/${submittedUsername}`);
-
-      if (!res.ok) {
-        throw new Error("User not found");
-      }
-
-      const data = await res.json();
-      console.log(data);
-
-      return data;
-    },
+    queryFn: () => fetchGithubUser(submittedUsername),
     enabled: !!submittedUsername,
   });
 
@@ -45,25 +35,7 @@ const UserSearch = () => {
       {isLoading && <p className='status'>Loading...</p>}
       {isError && <p className='status error'>{error.message}</p>}
 
-      {data && (
-        <div className='user-card'>
-          <img
-            src={data.avatar_url}
-            alt={data.name}
-            className='avatar'
-          />
-          <h2>{data.name || data.login}</h2>
-          <p>{data.bio}</p>
-          <a
-            href={data.html_url}
-            className='profile-btn'
-            target='_blank'
-            rel='noopener noreferrer'>
-            <FaGithubAlt />
-            View Github Profile
-          </a>
-        </div>
-      )}
+      {data && <UserCard user={data} />}
     </>
   );
 };
